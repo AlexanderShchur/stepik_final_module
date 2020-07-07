31d73d08806a9e14ac69047b6af673cf49f53695
AlexanderShchur <alexander.shchur27@gmail.com>
2020-07-08 01:32:05 +0300

    added test class and pytest ini

--- test_main_page.py
+++ test_main_page.py
@@ -6,22 +6,21 @@ from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
-@pytest.mark.skip
-def test_guest_can_go_to_login_page1(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page1(self, browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-
-@pytest.mark.skip
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
@@ -43,9 +42,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page.is_basket_empty_check()
     basket_page.presence_of_empty_basket_alert()
 
-
 # def test_presence_login_register_form(browser):
 #     link = "http://selenium1py.pythonanywhere.com/"
 #     page = LoginPage(browser, link)
 #     page.should_be_login_page()
-
